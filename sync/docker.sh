b682dda3b8987298035d6a277478cd3251bbcb4a
zhiqiang <zhiqiang2033@gmail.com>
2025-02-20 13:09:31 +0800

    fix: docker.sh

--- sync/docker.sh
+++ sync/docker.sh
@@ -1,24 +1,42 @@
 #!/usr/bin/env bash
 set -e
 
+# docker login registry.cn-hongkong.aliyuncs.com --username=username -p password
+# docker login hkccr.ccs.tencentyun.com --username=username -p password
 # curl -sSL https://raw.githubusercontent.com/izhiqiang/sh/main/sync/docker.sh | bash
 
-hubs=("hkccr.ccs.tencentyun.com/buildx/hub" "registry.cn-hongkong.aliyuncs.com/buildx/hub")
+
+hubs=("registry.cn-hongkong.aliyuncs.com/buildx/hub")
 
 mysql_versions=("5.7" "8.0" "8.1" "8.2" "8.3" "8.4")
 redis_versions=("6.2" "7.4")
+
+function sync(){
+    local from_image=$1
+    local target_image=$2
+    echo "Downloading ${from_image}"
+    docker pull ${from_image}
+
+    echo "rename ${from_image} -> ${target_image}"
+    docker tag ${from_image} ${target_image}
+
+    echo "push ${target_image}"
+    docker push ${target_image}
+
+    echo "deldete ${target_image}"
+    docker rmi ${from_image}
+
+    echo "deldete ${target_image}"
+    docker rmi ${target_image}
+}
 
 function sync_mysql() {
     local hub=$1
     for version in "${mysql_versions[@]}"
     do
-        local image="mysql:${version}"
+        local from_image="mysql:${version}"
         local target_image="${hub}:mysql-${version}"
-        echo "Downloading ${image}"
-        docker pull ${image}
-        echo "rename ${image} -> ${target_image}"
-        echo "push ${target_image}"
-        docker push ${target_image}
+        sync $from_image $target_image
     done
 }
 
@@ -26,21 +44,18 @@ function sync_redis() {
     local hub=$1
     for version in "${redis_versions[@]}"
     do
-        local image="redis:${version}"
+        local from_image="redis:${version}"
         local target_image="${hub}:redis-${version}"
-        echo "Downloading ${image}"
-        docker pull ${image}
-        echo "rename ${image} -> ${target_image}"
-        echo "push ${target_image}"
-        docker push ${target_image}
+        sync $from_image $target_image
     done
 }
 
 
-for hub in "${hubs[@]}"
-do
-    sync_mysql ${hub}
-    sync_redis ${hub}
-done
 
+sync_mysql registry.cn-hongkong.aliyuncs.com/buildx/hub
+sync_redis registry.cn-hongkong.aliyuncs.com/buildx/hub
+sync_mysql hkccr.ccs.tencentyun.com/buildx/hub
+sync_redis hkccr.ccs.tencentyun.com/buildx/hub
 
+sync zhiqiangwang/spug:latest registry.cn-hongkong.aliyuncs.com/buildx/soft:spug
+sync zhiqiangwang/spug:latest hkccr.ccs.tencentyun.com/buildx/soft:spug
